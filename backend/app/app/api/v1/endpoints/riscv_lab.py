c6f9e8ad46bc7d746b7b0aa63f93cf8d715cc71e
Camila Alvarez <cam.alvarez.i@gmail.com>
2025-04-16 15:41:12 -0400

    refactor(tuxsuite): Project restructuring
    Signed-off-by: Camila Alvarez <cam.alvarez.i@gmail.com>

--- backend/app/app/api/v1/endpoints/riscv_lab.py
+++ backend/app/app/api/v1/endpoints/riscv_lab.py
@@ -1,7 +1,7 @@
-from app.db import SessionDep
-from app.db.tests import ScheduledTests
+from app.core.db import SessionDep
+from app.models.tests import ScheduledTests
 from fastapi import APIRouter
-from app.schemas.request_schema import TuxSuiteTestSuite
+from app.schemas.tuxsuite import TuxSuiteTestSuite
 from app.services.tuxsuite_service import run_tuxsuite_tests
 
 
@@ -9,6 +9,11 @@ router = APIRouter()
 
 router.post("/run-tests", status_code=204)
 def run_tests(tests_data: TuxSuiteTestSuite, session: SessionDep):
+    """
+    Schedules tests in tuxsuite.
+    We only scheduledthe tests that have not been run for before for the build.
+    """
+    # TODO: should not run tests that have already been run
     test_uid = run_tuxsuite_tests(tests_data.kernel_image_url, tests_data.modules_url, tests_data.tests, "qemu-riscv64")
     scheduled_test = ScheduledTests(test_uid=test_uid, build_id=tests_data.build_id)
     session.add(scheduled_test)
--- backend/app/app/api/v1/endpoints/tuxsuite.py
+++ backend/app/app/api/v1/endpoints/tuxsuite.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Header
-
-from app.schemas.request_schema import TuxSuiteTestRequest
+from app.models.tests import ScheduledTests
+from app.schemas.tuxsuite import TuxSuiteTestRequest
 
 router = APIRouter()
 
@@ -9,9 +9,13 @@ router = APIRouter()
 @router.post("/test-callback", status_code=204)
 async def test_callback(x_tux_payload_signature: Annotated[str | None, Header()], request: TuxSuiteTestRequest):
     """
-    Callback for tuxsuite test 
+    Callback for tuxsuite test.
+    It obtains the test from the database (to get its build id), stores the results in 
+    the TestResults table and marks the test as finished.
     """
     print(x_tux_payload_signature)
+    parsed_test_results = None
+    # We o
     #body = await request.json()
     # It returns the results site in download_url, the you have to do
     # body[download_url]+ results.json
