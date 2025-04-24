cf300d0109c515cc698e5dbe0ec7be52842f9d78
Camila Alvarez <cam.alvarez.i@gmail.com>
2025-04-24 10:25:31 -0400

    fix(submit): Remove unnecessary parameter in parser
    Signed-off-by: Camila Alvarez <cam.alvarez.i@gmail.com>

--- backend/app/app/services/runner_service.py
+++ backend/app/app/services/runner_service.py
@@ -4,11 +4,11 @@ from app.services.kcidb_services import KCIDBTestSubmission
 from app.utils.test_parser import generate_test_id, get_test_path
 
 
-async def parse_results2kcidb(tests_results: RunnerTestsResults, stored_test: ScheduledTest) -> list[KCIDBTestSubmission]:
+async def parse_results2kcidb(tests_results: RunnerTestsResults) -> list[KCIDBTestSubmission]:
     parsed_results = []
     for test in tests_results.tests:
         path = get_test_path(test.test_collection, test)
-        test_id = generate_test_id(stored_test.test_uid, test.test_collection, test.test_name)
-        parsed_results.append(KCIDBTestSubmission(path, test.result, test.logs, test_id, stored_test.build_id))
+        test_id = generate_test_id(tests_results.test_uid, test.test_collection, test.test_name)
+        parsed_results.append(KCIDBTestSubmission(path, test.result, test.logs, test_id, tests_results.build_id))
     
     return parsed_results
