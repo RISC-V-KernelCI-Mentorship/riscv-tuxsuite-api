f1574814cc0b2b409f5eb2d3108ef5f3cbe9de5f
Camila Alvarez <cam.alvarez.i@gmail.com>
2025-05-02 14:47:57 -0400

    fix(middleware): Modify regex in middleware
    The reges was not fixing the invalid escape sequence correctly
    
    Signed-off-by: Camila Alvarez <cam.alvarez.i@gmail.com>

--- backend/app/app/middleware/json_middleware.py
+++ backend/app/app/middleware/json_middleware.py
@@ -7,7 +7,7 @@ class ModifyBuildCallbackBodyMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         if 'tuxsuite/callback/build' in str(request.url):
             body = await request.body()
-            fixed_body = re.sub(r'\\"', '"', body.decode("utf-8"))
+            fixed_body = re.sub(r'\"', '"', body.decode("utf-8"))
             fixed_body = re.sub(r"\\'", "'", fixed_body)
             request._body = fixed_body.encode("utf-8")
         return await call_next(request)
